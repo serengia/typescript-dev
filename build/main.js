d5606b4f22e25834ed97e89e367305c07defb91c
James Serengia <jamesserengia@gmail.com>
2024-03-27 11:51:14 +0300

    Add index signature

--- build/main.js
+++ build/main.js
@@ -1,3 +1,51 @@
 "use strict";
 let myName = "James Serengia";
-console.log(myName);
+// console.log(myName);
+// class Coder {
+//   secondLang!: string;
+//   name: string;
+//   age: number;
+//   city: string;
+//   constructor(name: string, age: number, city: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.city = city;
+//   }
+//   greet() {
+//     console.log(`Hello. My name is ${this.name}, I am ${this.age} years old`);
+//     console.log(`I live in ${this.city} Kenya`);
+//   }
+// }
+// const serecode = new Coder("James", 30, "Nairobi");
+// serecode.greet();
+class Coder {
+    //   name: string;
+    //   age: number;
+    //   city: string;
+    constructor(name, age, city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    greet() {
+        console.log(`Hello. My name is ${this.name}, I am ${this.age} years old`);
+        console.log(`I live in ${this.city} Kenya`);
+    }
+}
+const jamesTransactions = {
+    Book: 12,
+    Pizza: 13,
+    Transport: 100,
+};
+function calculateExpenditure(transactions) {
+    let total = 0;
+    for (const trans in transactions) {
+        total += transactions[trans];
+    }
+    return total;
+}
+const total = calculateExpenditure(jamesTransactions);
+console.log(total);
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,68 @@
 let myName = "James Serengia";
-console.log(myName);
+// console.log(myName);
+
+// class Coder {
+//   secondLang!: string;
+
+//   name: string;
+//   age: number;
+//   city: string;
+
+//   constructor(name: string, age: number, city: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.city = city;
+//   }
+
+//   greet() {
+//     console.log(`Hello. My name is ${this.name}, I am ${this.age} years old`);
+//     console.log(`I live in ${this.city} Kenya`);
+//   }
+// }
+
+// const serecode = new Coder("James", 30, "Nairobi");
+// serecode.greet();
+
+class Coder {
+  secondLang!: string;
+
+  //   name: string;
+  //   age: number;
+  //   city: string;
+
+  constructor(public name: string, private age: number, readonly city: string) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+  }
+
+  greet() {
+    console.log(`Hello. My name is ${this.name}, I am ${this.age} years old`);
+    console.log(`I live in ${this.city} Kenya`);
+  }
+}
+
+// const serecode = new Coder("James", 30, "Nairobi");
+// serecode.greet();
+
+// Index signature
+interface TransactionInt {
+  [index: string]: number;
+}
+
+const jamesTransactions: TransactionInt = {
+  Book: 12,
+  Pizza: 13,
+  Transport: 100,
+};
+
+function calculateExpenditure(transactions: TransactionInt): number {
+  let total = 0;
+  for (const trans in transactions) {
+    total += transactions[trans];
+  }
+  return total;
+}
+
+const total = calculateExpenditure(jamesTransactions);
+console.log(total);
